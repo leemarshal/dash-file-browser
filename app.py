d2baf10565fc625c75c64ccffa0f95b1495b89f8
MJ <alswns2631@gmail.com>
2023-05-22 18:35:11 +0900

    add delete branch option

--- app.py
+++ app.py
@@ -784,6 +784,30 @@ def toggle_popup(open_clicks, close_clicks, is_open, b1, b2, b3, b4):
         return True, branch, 0, 0
     return False, [], 0, 0
 
+@app.callback(
+    Output('b1', 'n_clicks'),
+    Output('delete_popup', 'is_open'),
+    Output('delete_popup', 'children'),
+    Input('delete_branch', 'n_clicks'),
+    State('branch_dropdown', 'value'),
+    State('cwd', 'children'),
+    State('b1', 'n_clicks'),
+)
+def delete_branch(click, value, cwd, b1):
+    if value:
+        if value[0] == "*":
+            value = value[1:].strip()
+        os.system('cd ' + str(Path(cwd)))
+        command = ["git", "branch", "-d", value]
+        data = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+        if not str(data.stderr):
+            data = data.stdout
+        else:
+            data = data.stderr
+        if not data:
+            data = 'delete branch ' + value
+        return b1 + 1, True, str(data)
+    return b1, False, ''
 
 if __name__ == '__main__':
     app.run_server(debug=True)
